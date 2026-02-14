0c802a4f8586e6c1e185e4a499d254a67467b891
NDERAGAKURA Alain Charbel <cnderagakura@bije.bi>
2026-02-14 14:20:41 +0200

    'ok'

--- indexold.php
+++ indexold.php
@@ -414,8 +414,8 @@
             <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; Museke mwese <?=date('Y')?></p></div>
         </footer>
         <!-- Bootstrap core JS-->
-        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <script src="assets/js/scripts.js"></script>
     </body>
 </html>
